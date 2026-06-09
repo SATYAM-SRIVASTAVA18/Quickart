4e3bc31f8725134908d42bb5dbd6770fb0ea58af
SATYAM-SRIVASTAVA18 <satyamsrivastava7869@gmail.com>
2026-06-09 17:25:13 +0530

    Fix JWT expire

--- fronted/src/component/Navbar.jsx
+++ fronted/src/component/Navbar.jsx
@@ -24,14 +24,21 @@ export default function Navbar() {
   const cart =
     JSON.parse(localStorage.getItem("cart")) || [];
 
-  const handleLogout = () => {
+  // const handleLogout = () => {
 
+  //   localStorage.removeItem("token");
+  //   localStorage.removeItem("user");
+
+  //   setUser(null);
+  //   navigate("/login");
+  // };
+
+  const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
-
     setUser(null);
-
-    navigate("/login");
+    navigate("/");
+    window.location.reload();
   };
 
   const handleSearch = async (e) => {
